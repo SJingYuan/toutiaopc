93dfb7729c725b2972d495a14b9080440d2558bb
JingYuan <JingYuanan_4@163.com>
2020-02-29 17:06:41 +0800

    完成登陆页面样式

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'// 引入模块
-import 'element-ui/lib/theme-chalk/index'// 引入样式
+import 'element-ui/lib/theme-chalk/index.css'// 引入样式
 import '@/styles/index.less'
 Vue.config.productionTip = false
 Vue.use(ElementUI)// 全局注册
---
+++ src/views/login/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="login">
+    <!-- 表单 -->
+    <el-card class="login-card">
+      <!-- 内容 -->
+      <!-- 头部 -->
+      <div class="title">
+        <img src="../../assets/img/logo_index.png" alt />
+      </div>
+      <!-- 表单 -->
+      <el-form style="margin-top:20px">
+        <!-- 表单容器 -->
+        <el-form-item>
+          <!-- 表单域 -->
+          <el-input placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <!-- 验证码 -->
+        <el-form-item>
+          <el-input style="width:60%" placeholder="请输入验证码"></el-input>
+          <!-- 按钮 -->
+          <el-button style="float:right" plain>发送验证码</el-button>
+        </el-form-item>
+        <!-- 表单域 -->
+        <el-form-item>
+          <el-checkbox>我已阅读同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item>
+          <el-button style="width:100%" type="primary">登陆</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script >
+export default {}
+</script>
+
+<style  lang="less" scoped>
+.login {
+  background-image: url("../../assets/img/joke.jpg");
+  height: 100vh;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-card {
+    width: 440px;
+    height: 340px;
+    .title {
+      text-align: center;
+      img {
+        height: 30px;
+      }
+    }
+  }
+}
+</style>
