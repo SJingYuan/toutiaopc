366d74bc8af6077140b1d85a376535e5cb76025d
JingYuan <JingYuanan_4@163.com>
2020-03-02 15:50:11 +0800

    主页布局完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>我是主页</div>
+  <!-- 容器 -->
+  <el-container>
+    <!-- 侧边容器 -->
+    <el-aside>
+      <h1>左侧导航</h1>
+    </el-aside>
+    <!-- 右侧容器 -->
+    <el-container>
+      <!-- 右侧顶部 -->
+      <el-header>
+        <h1>右侧顶部</h1>
+      </el-header>
+      <!-- 右侧内容区域 -->
+      <el-main>
+        <h1>右侧内容</h1>
+        <!-- 二级容器 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
