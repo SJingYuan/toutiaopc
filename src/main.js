5f69d01579e867a58c545d10af464d05ec47f711
JingYuan <JingYuanan_4@163.com>
2020-03-02 14:52:12 +0800

    完成手机验证

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import ElementUI from 'element-ui'// 引入模块
-import 'element-ui/lib/theme-chalk/index.css'// 引入样式
-import '@/styles/index.less'
+import ElementUI from 'element-ui' // 引入模块
+import axios from 'axios' // 引入axios
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import '@/styles/index.less' // 引入样式
 Vue.config.productionTip = false
-Vue.use(ElementUI)// 全局注册
+Vue.use(ElementUI) // 全局注册element-ui\
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 配置公共的请求头地址
+Vue.prototype.$axios = axios // 将axios赋值给 Vue对象的原型属性
 new Vue({
   router,
   render: h => h(App)
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,56 +2,124 @@
   <div class="login">
     <!-- 表单 -->
     <el-card class="login-card">
-      <!-- 内容 -->
-      <!-- 头部 -->
+      <!-- 表单内容 -->
+      <!-- 头部logo部分 -->
       <div class="title">
         <img src="../../assets/img/logo_index.png" alt />
       </div>
-      <!-- 表单 -->
-      <el-form style="margin-top:20px">
-        <!-- 表单容器 -->
-        <el-form-item>
-          <!-- 表单域 -->
-          <el-input placeholder="请输入手机号"></el-input>
+      <!-- 表单 绑定model属性  绑定rules属性(表单验证规则) ref 给el-form一个属性-->
+      <el-form ref="loginForm" :model="loginForm" :rules="loginRules" style="margin-top:20px">
+        <!-- 表单容器 设置prop属性 prop表示要校验的字段名-->
+        <el-form-item prop="mobile">
+          <!-- 表单域  v-model双向绑定 -->
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
         <!-- 验证码 -->
-        <el-form-item>
-          <el-input style="width:60%" placeholder="请输入验证码"></el-input>
-          <!-- 按钮 -->
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" style="width:60%" placeholder="请输入验证码"></el-input>
+          <!-- 放置一个按钮 -->
           <el-button style="float:right" plain>发送验证码</el-button>
         </el-form-item>
         <!-- 表单域 -->
-        <el-form-item>
-          <el-checkbox>我已阅读同意用户协议和隐私条款</el-checkbox>
+        <el-form-item prop="checked">
+          <!-- 是否同意被人家坑 -->
+          <el-checkbox v-model="loginForm.checked">我已阅读同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <!-- 按钮 -->
         <el-form-item>
-          <el-button style="width:100%" type="primary">登陆</el-button>
+          <el-button @click="login" style="width:100%" type="primary">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
   </div>
 </template>
 
-<script >
-export default {}
+<script>
+export default {
+  data () {
+    return {
+      // 登录表单的数据
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        checked: false // 是否同意用户协议
+      },
+      // 定义表单的验证规则
+      loginRules: {
+        // required 如果为true表示该字段必填
+        mobile: [
+          { required: true, message: '您的手机号不能为空' },
+          {
+            pattern: /^1[3-9]\d{9}$/, // 正则表达式
+            message: '您的手机号格式不正确'
+          }
+        ],
+        code: [
+          { required: true, message: '您的验证码不能为空' },
+          {
+            pattern: /^\d{6}$/, // 要求6个数字
+            message: '验证码应该是6位数字'
+          }
+        ],
+        // 自定义校验  required不能校验true/false
+        checked: [
+          {
+            validator: function (rule, value, callback) {
+              // rule是当前的校验规则
+              value
+                ? callback()
+                : callback(new Error('您必须同意我们的霸王条款'))
+            }
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.loginForm.validate().then(() => {
+        this.$axios({
+          url: '/authorizations',
+          data: { ...this.loginForm, checked: null },
+          method: 'post'
+        })
+          .then(res => {
+            console.log(res)
+          })
+          .catch(() => {})
+      })
+    }
+  }
+}
 </script>
 
-<style  lang="less" scoped>
+<style lang='less' scoped>
+// 加了scoped属性 就只会对当前自己的组件起作用
+// 如果需要写less 需要在style标签中 lang='less'
 .login {
-  background-image: url("../../assets/img/joke.jpg");
-  height: 100vh;
-  background-size: cover;
+  // background-image: url('../../assets/img/back.jpg');
+  height: 100vh; // 当前屏幕可视区域分成100份
   display: flex;
   justify-content: center;
   align-items: center;
+  &:before {
+    content: "123";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    background-image: url("../../assets/img/login_bg.jpg");
+    filter: blur(5px);
+    background-size: cover;
+  }
   .login-card {
+    background: #fff;
+    z-index: 2;
     width: 440px;
     height: 340px;
     .title {
       text-align: center;
       img {
-        height: 30px;
+        height: 40px;
       }
     }
   }
