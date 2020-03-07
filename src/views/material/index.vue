d0c52ba052f58131f659c8448c8f71fccbcbd7c7
JingYuan <JingYuanan_4@163.com>
2020-03-07 11:24:25 +0800

    完成素材管理

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -13,19 +13,19 @@
     <el-tabs v-model="activeName" @tab-click="changeTab">
       <el-tab-pane label="全部素材" name="all">
         <div class="imgList">
-          <el-card class="imgCrad" v-for="item in list" :key="item.id">
-            <img :src="item.url" alt />
+          <el-card class="imgCrad" v-for="(item,index) in list" :key="item.id">
+            <img :src="item.url" alt="" @click="selectImg(index)">
             <el-row class="yang" type="flex" justify="space-around" align="middle">
-              <i class="el-icon-star-on"></i>
-              <i class="el-icon-delete-solid"></i>
+              <i class="el-icon-star-on" @click="collectOrCancel(item)" :style="{color:item.is_collected?'red':'black'}"></i>
+              <i @click="deleteImg(item)"  class="el-icon-delete-solid"></i>
             </el-row>
           </el-card>
         </div>
       </el-tab-pane>
       <el-tab-pane label="收藏素材" name="collect">
         <div class="imgList">
-          <el-card class="imgCrad" v-for="item in list" :key="item.id">
-            <img :src="item.url" alt />
+          <el-card class="imgCrad" v-for="(item,index) in list" :key="item.id">
+            <img :src="item.url" alt=""   @click="selectImg(index)">
           </el-card>
         </div>
       </el-tab-pane>
@@ -41,6 +41,13 @@
         @current_change="changeCurrent"
       ></el-pagination>
     </el-row>
+    <el-dialog @opened="openEnd" :visible="dialogVisible" @close="dialogVisible=false">
+      <el-carousel ref="myCarousel" indicator-position="outside" height="400px">
+        <el-carousel-item v-for="item in list" :key="item.id">
+          <img style="width:100%; height:100%" :src="item.url" alt="" >
+        </el-carousel-item>
+      </el-carousel>
+    </el-dialog>
   </el-card>
 </template>
 
@@ -54,10 +61,45 @@ export default {
         currentPage: 1,
         total: 0,
         pageSize: 8
-      }
+      },
+      dialogVisible: false,
+      clickIndex: -1
     }
   },
   methods: {
+    openEnd () {
+      this.$refs.myCarousel.setActiveItem(this.clickIndex)
+    },
+    // 点击图片时调用
+    selectImg (index) {
+      this.clickIndex = index
+      this.dialogVisible = true
+    },
+    collectOrCancel (row) {
+      this.$axios({
+        method: 'put',
+        url: `/user/images/${row.id}`,
+        data: {
+          collect: !row.is_collected
+        }
+      }).then(() => {
+        this.getData()
+      }).catch(() => {
+        this.$message.error('收藏失败')
+      })
+    },
+    deleteImg (row) {
+      this.$confirm('确定要删除嘛', '提示').then(() => {
+        this.$axios({
+          method: 'delete',
+          url: `/user/images/${row.id}`
+        }).then(() => {
+          this.getData()
+        }).catch(() => {
+          this.$message.error('删除失败')
+        })
+      })
+    },
     uploadImg (params) {
       const data = new FormData()
       data.append('image', params.file)
@@ -84,7 +126,8 @@ export default {
           collect: this.activeName === 'collect',
           page: this.page.currentPage,
           per_page: this.page.pageSize
-        }
+        },
+        data: {}
       }).then(res => {
         this.list = res.data.results
         this.page.total = res.data.total_count
